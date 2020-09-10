eaec00235c8c9474bfeec132de7c6a7ac4892933
Gergana Belcheva <gergana.belcheva@gmail.com>
2020-09-10 12:09:41 +0300

    Adhere to PEP8

--- tests/services/test_validator.py
+++ tests/services/test_validator.py
@@ -3,12 +3,12 @@ from pytest import raises
 from services import validator
 
 
-
 def test_latitude_valid():
     latitude = -90.
     is_valid = validator.validate_lat(latitude)
 
     assert is_valid
+
 
 def test_latitude_too_big_invalid():
     latitude = 91.
@@ -16,11 +16,13 @@ def test_latitude_too_big_invalid():
 
     assert not is_valid
 
+
 def test_latitude_too_small_invalid():
     latitude = -91.
     is_valid = validator.validate_lat(latitude)
 
     assert not is_valid
+
 
 def test_longitude_valid():
     longitude = 180.
@@ -28,11 +30,13 @@ def test_longitude_valid():
 
     assert is_valid
 
+
 def test_longitude_too_big_invalid():
     longitude = 181.
     is_valid = validator.validate_long(longitude)
 
     assert not is_valid
+
 
 def test_longitude_too_small_invalid():
     longitude = -181.
